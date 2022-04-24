4103898b2300e929d0154dcea5880d81436cfaa9
hxAri <90847846+hxAri@users.noreply.github.com>
2022-04-24 18:13:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,10 @@
 <?php
 
-use Util\Octancle;
-
 /* Show all error reports. */
 error_reporting( E_ALL );
 
 /* Get server document root. */
 $root = $_SERVER['DOCUMENT_ROOT'];
-
-/*
- * Register Auto Load.
- *
- * Automatic loading of files required for a project or application.
- * This includes the files required for the application without explicitly
- * including them with the [include] or [require] functions.
- */
-require "$root/vendor/autoload.php";
-
-// Import all Yume built-in Functions.
-require "$root/system/helpers/.php";
 
 // Get array for constant.
 require "$root/system/__init__/constant.php";
@@ -27,9 +13,11 @@ require "$root/system/__init__/constant.php";
 require "$root/system/__init__/bootstrap.php";
 
 // Replace Name space.
-Util\Function\replace( Util\Function\tree( "/" ), [] );
+Yume\Func\replace( Yume\Func\tree( "/" ), [
+	
+]);
 
 // Run the application.
-Util\Octancle\Runtime\Runtime::app();
+Yume\Util\Runtime\Runtime::app();
 
 ?>
