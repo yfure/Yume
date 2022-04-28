09b12b862420ade47a8765d74d45769b8306d70b
hxAri <90847846+hxAri@users.noreply.github.com>
2022-04-29 04:32:48 +0700

    Create ErrorProvider.php

---
+++ system/builtin/Util/Error/ErrorProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Yume\Util\Error;
+
+use function Yume\Func\path;
+
+use Yume\Util\IO;
+use Yume\Util\Himei;
+
+/*
+ * ErrorProvider utility class.
+ *
+ * @package Yume\Util\Error
+ */
+abstract class ErrorProvider
+{
+    
+    /*
+     * @inheritdoc ErrorInterface
+     *
+     */
+    public static function write( String $dir, Array $error ): Void
+    {
+        $error['timestamp'] = Himei\Application::$object->dateTime->getTimestamp();
+        
+        // If the error store has not been created. **/
+        if( file_exists( path( noteware: $dir ) ) === False )
+        {
+            // Creating an error log directory. **/
+            mkdir( path( noteware: $dir ) );
+        }
+        
+        // Parse error to tree string.
+        $data = Himei\Tree::tree( $error, type: Himei\Tree::POINT );
+        
+        // Create file name by date time.
+        $path = path( noteware: "{$dir}/" );
+        
+        // Write file.
+        IO\File\File::write( $path . Himei\Application::$object->dateTime->format( "d.M-Y.\l\o\g" ), $data, "a" );
+        
+    }
+    
+}
+
+?>
