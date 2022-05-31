1ab6ec178be5a79b4874b425d6a95c67a68d10b0
Ari | Backdev <90847846+hxAri@users.noreply.github.com>
2022-06-01 00:34:10 +0700

    Update routes.php

--- system/routes/routes.php
+++ system/routes/routes.php
@@ -2,14 +2,7 @@
 
 use function Yume\Func\view;
 
-use App\HTTP\Controllers;
-use App\Models;
-
-use Yume\Util\Database;
-use Yume\Util\Himei;
 use Yume\Util\HTTP;
-use Yume\Util\IO;
-use Yume\Util\Trouble;
 
 HTTP\Route::add( "GET", "/", fn() => view( "views.welcome" ) );
 
