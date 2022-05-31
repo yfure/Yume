93fb397fd5ca12c73b6b47de5d04f0b02fc97085
Ari | Backdev <90847846+hxAri@users.noreply.github.com>
2022-06-01 00:52:47 +0700

    Create Iterator.php

---
+++ system/builtin/Util/Himei/Iterator.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Yume\Util\Himei;
+
+trait Iterator
+{
+    
+    /*
+     * Current element value.
+     *
+     * @values Int
+     */
+    protected $position;
+    
+    use Overload;
+    
+    /*
+     * Return the current element.
+     *
+     * @access Public
+     *
+     * @return Mixed
+     */
+    public function current(): Mixed
+    {
+        return( $this->data[$this->position] );
+    }
+    
+    /*
+     * Rewind the Iterator to the first element.
+     *
+     * @access Public
+     *
+     * @return Void
+     */
+    public function rewind(): Void
+    {
+        $this->position = 0;
+    }
+    
+    /*
+     * Checks if current position is valid.
+     *
+     * @access Public
+     *
+     * @return Bool
+     */
+    public function valid(): Bool
+    {
+        return( isset( $this->data[$this->position] ) );
+    }
+    
+    /*
+     * Get value element.
+     *
+     * @access Public
+     *
+     * @return Mixed
+     */
+    public function value(): Mixed
+    {
+        return( $this->data[$this->position] );
+    }
+    
+    /*
+     * Move forward to next element.
+     *
+     * @access Public
+     *
+     * @return Void
+     */
+    public function next(): Void
+    {
+        $this->position++;
+    }
+    
+    /*
+     * Return the key of the current element.
+     *
+     * @access Public
+     *
+     * @return Mixed
+     */
+    public function key(): Mixed
+    {
+        return( $this->position );
+    }
+    
+}
+
+?>
