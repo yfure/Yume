c8fe282a5be07164a6976e5f82fee197881f8b1e
Ari | Backdev <90847846+hxAri@users.noreply.github.com>
2022-06-01 00:53:38 +0700

    Create Overload.php

---
+++ system/builtin/Util/Himei/Overload.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Yume\Util\Himei;
+
+trait Overload
+{
+    
+    /*
+     * Location for overloaded data.
+     *
+     * @access Protected
+     *
+     * @values Array
+     */
+    protected Array | Data $data = [];
+    
+}
+
+?>
