0463f1b8a2bdaebd623ff26ebf38460a781a9c9f
Ari | Backdev <90847846+hxAri@users.noreply.github.com>
2022-06-01 00:52:22 +0700

    Create Hairetsu.php

---
+++ system/builtin/Util/Himei/Hairetsu.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Yume\Util\Himei;
+
+interface Hairetsu
+{
+    
+    /*
+     * The Class Constructor
+     *
+     * @access Public
+     *
+     * @params Array <data>
+     *
+     * @return Static
+     */
+    public function __construct( Array $data = [] );
+    
+}
+
+?>
