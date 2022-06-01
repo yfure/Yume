ec7d882fbc05de8ea27810416bc4fe8fa16ff95d
hxAri <ari160824@gmail.com>
2022-06-01 07:42:40 +0700

    Create/Update/Delete

--- public/index.php
+++ public/index.php
@@ -1,5 +1,25 @@
 <?php
 
-print_r( scandir( "." ) );
+/*
+ * Yume PHP Framework.
+ *
+ * @author hxAri
+ * @create 05.02-2022
+ * @update 01.06-2022
+ * @github https://github.com/hxAri/{Yume}
+ *
+ * Yume is a simple framework for building Websites
+ * built using the PHP Programming Language, it already
+ * provides several built-in packages that are ready to
+ * use but some packages are under development and code
+ * repair. You can also install other PHP libraries if
+ * the package is incomplete or incomplete.
+ *
+ * All Yume source code is under the MIT license,
+ * please see the original for more details.
+ *
+ */
+
+
 
 ?>
