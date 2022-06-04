a7fab6a8d41d64e001a6f592525c05815256c40d
hxAri <ari160824@gmail.com>
2022-06-05 03:50:24 +0700

    Update Trouble/Memew/Sutakku/SutakkuInterface

--- system/buffer/framework/Trouble/Memew/Sutakku/SutakkuInterface.php
+++ system/buffer/framework/Trouble/Memew/Sutakku/SutakkuInterface.php
@@ -2,9 +2,36 @@
 
 namespace Yume\Kama\Obi\Trouble\Memew\Sutakku;
 
+use Throwable;
+
 interface SutakkuInterface
 {
+    /*
+     * Get Exception thrown
+     *
+     * @access Public
+     *
+     * @return Throwable
+     */
+    public function getObject(): Throwable;
     
+    /*
+     * Get Exception stack trace.
+     *
+     * @access Public
+     *
+     * @return Array
+     */
+    public function getStacks(): Array;
+    
+    /*
+     * Get Exception previous
+     *
+     * @access Public
+     *
+     * @return Array, & Null
+     */
+    public function getPrevis(): ? Array;
 }
 
 ?>
