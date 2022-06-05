ecabd19fa9b909ee57bd7ba233f1aae60ad9f4e7
hxAri <ari160824@gmail.com>
2022-06-05 21:52:06 +0700

    Update bootstrap

--- public/bootstrap.php
+++ public/bootstrap.php
@@ -1,33 +1,27 @@
 <?php
 
 /*
- * Bootrapping file for Yume Framework.
+ * Bootstrap
  *
- * Takes various functions as well as built-in classes from yume.
- * It will also check if the user is using php version ^8.0.
+ * Bootstrapping for the framework.
  *
- * @author hxAri
- * @license Under MIT
  */
-try {
-    if( PHP_VERSION < $v = "8.0.6" )
-    {
-        throw new RuntimeException( "Oops sorry it looks like you are using a PHP version\nbelow {$v} please upgrade the PHP version to {$v} or higher." );
-    }
-} catch( RuntimeException $e ) {
-    echo $e::class . ": {$e->getMessage()}\n{$e->getFile()} on line {$e->getLine()}";
-}
-
-$constant = [
+if( version_compare( PHP_VERSION, "8.0.0" ) > 0 )
+{
     
-    'YUME' => [
+    /*
+     * Application constants.
+     *
+     * @values Array
+     */
+    $constants = [
         
         /*
          * Runtime application.
          *
          * @values Bool
          */
-        'isCommandLineInterface' => php_sapi_name() === "cli",
+        'CLI' => php_sapi_name() === "cli",
         
         /*
          * Dynamically base url.
@@ -41,15 +35,18 @@ $constant = [
             // Default base url.
             $baseURL = "http";
             
-            if( isset( $_SERVER['HTTP_HOST'] ) ) {
-                if( isset( $_SERVER['HTTPS'] ) ) {
-                    if( strtolower( $_SERVER['HTTPS'] ) == 'on' ) {
+            if( isset( $_SERVER['HTTP_HOST'] ) )
+            {
+                if( isset( $_SERVER['HTTPS'] ) )
+                {
+                    if( strtolower( $_SERVER['HTTPS'] ) == "on" )
+                    {
                         $baseURL = "https";
                     }
                 }
                 
                 // Get url webpage.
-                $baseURL .= '://'. $_SERVER['HTTP_HOST'];
+                $baseURL .= "://{$_SERVER['HTTP_HOST']}";
                 
                 // Replacimg script name.
                 $baseURL .= str_replace( basename( $_SERVER['SCRIPT_NAME'] ), "", $_SERVER['SCRIPT_NAME'] );
@@ -74,14 +71,9 @@ $constant = [
             // Get document path root server.
             $baseRoot = $_SERVER['DOCUMENT_ROOT'];
             
-            if( isCommandLineInterface )
-            {
-                $baseRoot = str_replace( "/system/__init__", "", __DIR__ );
-            }
-            
             // This will probably only apply to android
             // Users who run this via the "KSWEB" or "Termux" software.
-            $baseRoot = preg_replace( "/\/mnt\/sdcard\/|\/sdcard\//", "/storage/emulated/0/", $baseRoot );
+            $baseRoot = preg_replace( "/\/mnt\/sdcard\/|\/sdcard\//", "/storage/emulated/0/", CLI ? str_replace( "/public", "", __DIR__ ) : $baseRoot );
             
             return( $baseRoot );
         },
@@ -95,51 +87,18 @@ $constant = [
          */
         'BASE_PATH' => fn() => preg_replace( "/[\/|\\\](public|bootstrap)/i", "", BASE_ROOT ),
         
-        /*
-         * Application path.
-         *
-         * @values Callable
-         *
-         * @return String
-         */
-        'APP_PATH' => "/app",
-        'ASSETS_PATH' => "/assets",
-        'VENDOR_PATH' => "/vendor",
-        'CONFIG_PATH' => "/configs",
-        'SYSTEM_PATH' => "/system",
-        'FUNC_PATH' => "/system/helper",
-        'ROUTES_PATH' => "/storage/routes",
-        'UTIL_PATH' => "/system/builtin/Util",
-        'STORAGE_PATH' => "/storage",
-        'LOG_PATH' => "/storage/logs",
-        'DATABASE_PATH' => "/storage/database",
-        'LANGUAGE_PATH' => "/storage/language",
-        
-        /*
-         * Application environment.
-         *
-         * @values String
-         */
-        'ENVIRONMENT' => "development"
-        
-    ],
+    ];
     
-    'SERVER' => $_SERVER
-    
-];
-
-/*
- * Define super global constants based element name.
- *
- * You can also implement your constant names and values easily.
- * See ./const file for define new constant.
- */
-foreach( $constant As $group => $array )
-{
-    foreach( $array As $const => $value )
+    foreach( $constants as $name => $value )
     {
-        define( ( $group === "SERVER" ? "SERVER_" : "" ) . $const, is_callable( $value ) ? call_user_func( $value ) : $value );
+        if( defined( $name ) === False )
+        {
+            define( $name, is_callable( $value ) ? $value() : $value );
+        }
     }
+    
+} else {
+    throw new UnexpectedValueException( "PHP version not supported by framework." );
 }
 
 ?>
