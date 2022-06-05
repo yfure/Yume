188838f84ac0d75f705c8c33066662b816e09de2
hxAri <ari160824@gmail.com>
2022-06-05 21:54:10 +0700

    Update AoE/App

--- system/buffer/framework/AoE/App.php
+++ system/buffer/framework/AoE/App.php
@@ -1,97 +1,71 @@
 <?php
 
-namespace Yume\Kama\Obi\AoE
+namespace Yume\Kama\Obi\AoE;
+
+use DateTimeZone;
+use DateTime;
+
+final class App
 {
     
-    use DateTimeZone;
-    use DateTime;
+    /*
+     * ONSaved Instances
+     *
+     * @access Public Static
+     *
+     * @values Yume\Kama\Obi\Data
+     */
+    public static $object;
     
-    abstract class App
+    use Buffer\Config;
+    
+    /*
+     * Initialize app runtime.
+     *
+     * @access Public, Static
+     *
+     * @return Void
+     */
+    public static function buff(): Void
     {
         
         /*
-         * DateTime Class Instance.
+         * DateTimeZone
          *
-         * @access Public: Static
+         * Create a new instance of the DateTimeZone class,
+         * parameter values based on the application configuration.
          *
-         * @values DateTime
+         * @see configs/app
          */
-        public static $dateTime;
+        $dateTimeZone = new DateTimeZone( self::config( "timezone" ) );
         
         /*
-         * DateTimeZone Class Instance.
+         * DateTime
          *
-         * @access Public, Static
-         *
-         * @values DateTimeZone
+         * Set datetime by current timezone.
          */
-        public static $dateTimeZone;
+        $dateTime = new DateTime( "now", $dateTimeZone );
+        
+        self::$object = new Data([
+            "dateTimeZone" => $dateTimeZone,
+            "dateTime" => $dateTime
+        ]);
         
         /*
-         * Router Class Instance.
+         * Set user-defined error handler function.
          *
-         * @access Public, Static
-         *
-         * @values Yume\Kama\Obi\HTTP\Route\Router
+         * @see configs/app
          */
-        public static $router;
-        public static $object;
-        
-        use Traits\Config;
+        set_error_handler( self::config( "trouble.error.handler" ) );
         
         /*
-         * Initialize app runtime.
+         * Sets a user-defined exception handler function.
          *
-         * @access Public, Static
-         *
-         * @return Void
+         * @see configs/app
          */
-        public static function buff(): Void
-        {
-            
-            // Create instance.
-            self::$object = new Data;
-            
-            /*
-             * DateTime
-             * DateTimeZone
-             *
-             * Set time format based on configuration or
-             * You can change it in application configuration section.
-             */
-            self::$object->dateTime = new DateTime( "now", self::$object->dateTimeZone = new DateTimeZone( self::config( "timezone" ) ) );
-            
-            /*
-             * Set user-defined error handler function.
-             *
-             * @see configs/app
-             */
-            set_error_handler( self::config( "trouble.error.handler" ) );
-            
-            /*
-             * Sets a user-defined exception handler function.
-             *
-             * @see configs/app
-             */
-            set_exception_handler( self::config( "trouble.exception.handler" ) );
-            
-            try {
-                try {
-                    try {
-                        throw new \ArithmeticError( "Catch me!" );
-                    } catch( \ArithmeticError $e ) {
-                        throw new \AssertionError( "", 0, previous: $e );
-                    }
-                } catch( \AssertionError $e ) {
-                    throw new \RuntimeException( "", 0, previous: $e );
-                }
-            } catch ( \RuntimeException $e ) {
-                throw new \Error( "", 0, previous: $e );
-            }
-            
-        }
+        set_exception_handler( self::config( "trouble.exception.handler" ) );
         
-        public static function x() {}
+        
     }
     
 }
