53234c147003e701152e72ea31ea8574860463ad
hxAri <ari160824@gmail.com>
2022-06-05 22:00:37 +0700

    Update AoE/Buffer/Overload

---
+++ system/buffer/framework/AoE/Buffer/Overload.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Yume\Kama\Obi\AoE\Buffer;
+
+trait Overload
+{
+    
+    /*
+     * Location for overloaded data.
+     *
+     * @access Protected
+     *
+     * @values Array
+     */
+    protected Array | Data $data = [];
+    
+}
+
+?>
