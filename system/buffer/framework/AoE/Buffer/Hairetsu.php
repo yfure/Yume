d30ede841a3790c3cf35f1d8ff3e16f5c1cf028b
hxAri <ari160824@gmail.com>
2022-06-05 21:58:10 +0700

    Update AoE/Buffer/Hairetsu

---
+++ system/buffer/framework/AoE/Buffer/Hairetsu.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Yume\Kama\Obi\AoE\Buffer;
+
+interface Hairetsu
+{
+    
+    /*
+     * The Class Constructor
+     *
+     * @access Public
+     *
+     * @params Array <data>
+     *
+     * @return Static
+     */
+    public function __construct( Array $data = [] );
+    
+}
+
+?>
