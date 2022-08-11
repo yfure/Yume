bf0e8957cfef35faf4094b2d37e215192fcd5fce
hxAri <ari160824@gmail.com>
2022-08-11 18:42:25 +0700

    Update

--- app/HTTP/Controllers/Main.php
+++ app/HTTP/Controllers/Main.php
@@ -16,31 +16,31 @@ use Yume\Fure\HTTP;
  */
 class Main extends HTTP\Controller\Controller
 {
-	
-	/*
-	 * Construct method of class Main Controller.
-	 *
-	 * @access Public Instance
-	 *
-	 * @return Void
-	 */
-	public function __construct()
-	{
-		// ...
-	}
-	
-	/*
-	 * Main method of Main controller.
-	 *
-	 * @access Public
-	 *
-	 * @return Void
-	 */
-	public function main( HTTP\Routing\Route $route ): Void
-	{
-		echo "Welcome to Yume!";
-	}
-	
+    
+    /*
+     * Construct method of class Main Controller.
+     *
+     * @access Public Instance
+     *
+     * @return Void
+     */
+    public function __construct()
+    {
+        // ...
+    }
+    
+    /*
+     * Main method of Main controller.
+     *
+     * @access Public
+     *
+     * @return Void
+     */
+    public function main( HTTP\Routing\Route $route ): Void
+    {
+        echo "Welcome to Yume!";
+    }
+    
 }
 
 ?>
