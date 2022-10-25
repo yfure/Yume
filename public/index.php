76c939a3f0ee689686e594b72b35880928fb880b
hxAri <ari160824@gmail.com>
2022-10-25 17:08:55 +0700

    Update

---
+++ public/index.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * Yume PHP Framework.
+ *
+ * @author hxAri
+ * @create 05.02-2022
+ * @update -
+ * @github https://github.com/yfure/Yume
+ *
+ * Yume is a simple framework for building Websites
+ * built using the PHP Programming Language, it already
+ * provides several built-in packages that are ready to
+ * use but some packages are under development and code
+ * repair. You can also install other PHP libraries if
+ * the package is incomplete or incomplete.
+ *
+ * All Yume source code is under the MIT license,
+ * please see the original for more details.
+ */
+
+use Yume\Fure\App;
+
+define( "YUME_START", microtime( True ) );
+
+/*
+ * Register Auto Load.
+ *
+ * Automatic loading of files required for a project or application.
+ * This includes the files required for the application without
+ * explicitly including them with the include or require functions.
+ */
+require( $_SERVER['DOCUMENT_ROOT'] . "/vendor/autoload.php" );
+
+App\App::self();
+
+
+?>
--- system/buffer/bootstrap.php
+++ system/buffer/bootstrap.php
@@ -4,21 +4,24 @@
  * Bootstrap The Application.
  *
  * This process compares the minimum version of PHP used,
- * creates constants including application base path constants,
- * and loads environment variables.
+ * creates constants including application base path constants.
  */
 
 define( "YUME_VERSION", "3.0.4" );
 define( "YUME_PHP_VERSION", "8.1.10" );
 
+// Define Yume application context.
+define( "YUME_CONTEXT", PHP_SAPI );
+
 // Check whether the php version used is standard.
 if( version_compare( PHP_VERSION, YUME_PHP_VERSION, ">" ) )
 {
-	// Define base path application.
-	define( "BASE_PATH", str_replace( "/", DIRECTORY_SEPARATOR, __DIR__ ) );
-	
-	// Load application environment variables.
-	Yume\Fure\Support\Env\Env::self()->load();
+	// Check if basepath is undefined.
+	if( defined( "BASE_PATH" ) === False )
+	{
+		// Define base path application.
+		define( "BASE_PATH", str_replace( "/", DIRECTORY_SEPARATOR, $_SERVER['DOCUMENT_ROOT'] ) );
+	}
 }
 else {
 	exit( sprintf( "Your PHP Version is %s, You need PHP Version %s or higher to run Yume.", PHP_VERSION, YUME_PHP_VERSION ) );
