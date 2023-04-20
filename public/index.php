d354ab7df62847b973785f0af7100e2b1c954e7c
hxAri <ari160824@gmail.com>
2023-04-20 13:29:22 +0700

    Update

--- public/index.php
+++ public/index.php
@@ -8,15 +8,25 @@
  * @update -
  * @github https://github.com/yfure/Yume
  *
- * Yume is a simple framework for building Websites
- * built using the PHP Programming Language, it already
- * provides several built-in packages that are ready to
- * use but some packages are under development and code
- * repair. You can also install other PHP libraries if
- * the package is incomplete or incomplete.
+ * Copyright (c) 2022 Yume Framework
  *
- * All Yume source code is under the MIT license,
- * please see the original for more details.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 use Yume\Fure\App\App;
