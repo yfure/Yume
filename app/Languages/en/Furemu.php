3675bc89c3261de4bac0b27153705a9b5fad47b1
hxAri <ari160824@gmail.com>
2023-05-27 07:24:43 +0700

    Update

---
+++ app/Languages/en/Furemu.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * The Yume Framework Translations.
+ *
+ * This is a translation for English.
+ *
+ * @author Ari Setiawan (hxAri)
+ * @github https://github.com/hxAri
+ * @e-mail hxari@proton.me
+ */
+return([
+	"yume" => [
+		"fure" => [
+			//...import( Yume\Fure\Locale\Translation\en\Cache::class ),
+			//...import( Yume\Fure\Locale\Translation\en\Cookie::class ),
+			//...import( Yume\Fure\Locale\Translation\en\Env::class ),
+			...import( Yume\Fure\Locale\Translation\en\Error::class ),
+			//...import( Yume\Fure\Locale\Translation\en\Json::class ),
+			//...import( Yume\Fure\Locale\Translation\en\Logger::class ),
+			//...import( Yume\Fure\Locale\Translation\en\RegExp::class ),
+			//...import( Yume\Fure\Locale\Translation\en\Router::class ),
+			//...import( Yume\Fure\Locale\Translation\en\Session::class ),
+			//...import( Yume\Fure\Locale\Translation\en\Stream::class ),
+			//...import( Yume\Fure\Locale\Translation\en\::class )
+		]
+	]
+]);
+
+?>
